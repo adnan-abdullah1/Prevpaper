ac65d073ae22b7bf68205047dd69caf649143a66
root <root@localhost.localdomain>
2020-04-12 20:22:37 +0530

    had guess about upload

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ app=Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
-@app.route("/contact")
-def select_course():
-    return render_template("contact.html")
+@app.route("/upload")
+def upload_papers():
+    return render_template("upload.html")
 @app.route("/about")
 def About():
     return render_template("about.html")
 
+@app.route("/getpapers")
+def gwtpaper():
+    return  render_template("getpapers.html")
 
 app.run(debug=True,port=50)
