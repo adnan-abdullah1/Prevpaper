888d366e8081707842aa3714f0b2bc016d8eb52a
root <root@localhost.localdomain>
2020-04-14 18:14:47 +0530

    fetch value of dropdown and pas to other fxn

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 app=Flask(__name__)
 @app.route("/")
 def home():
@@ -13,5 +13,14 @@ def About():
 @app.route("/getpapers")
 def gwtpaper():
     return  render_template("getpapers.html")
+
+#below fxn gets value from drop of semster in upload paper
+@app.route("/paper_show" , methods=['GET', 'POST'])
+def return_sem():
+    select = request.form['sem']
+    select = request.form.get('sem')
+    #return(str(select)) # just to see what select is
+    print("hey fetched val is ",str(select))
+    return render_template('paper_show.html',param=str(select)) #param is parameter that i m passing to html page
 if __name__ == "__main__":
-    app.run(debug=True,port=1111)    
+    app.run(debug=True,port=1115)    
