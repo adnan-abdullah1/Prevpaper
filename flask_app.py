e85ac0e22525a147d423953e882d7e7229dca39d
addddd123 <adshe886@gmail.com>
2020-04-20 00:14:53 +0530

    corrected database

--- flask_app.py
+++ flask_app.py
@@ -9,7 +9,7 @@ import sqlalchemy.dialects.sqlite
 
 app=Flask(__name__)
 #registry.register('snowflake', 'snowflake.sqlalchemy', 'dialect')
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///previouspaper.db"#"mysql+pymyql://root:admin@127.0.0.1/btech"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///paper11.db"#"mysql+pymyql://root:admin@127.0.0.1/btech"
 
 db = SQLAlchemy(app)   #db object
 
@@ -18,38 +18,50 @@ class sem1(db.Model):
     batch = db.Column(db.Integer, nullable=True)
     
     papertype = db.Column(db.String(120), nullable=True)
-    paper = db.Column(db.String(120), nullable=False)
+    paper = db.Column(db.LargeBinary, nullable=False)
+    def __repr__(self):
+        return 'data is : ' + str(self.sno)
 class sem2(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     batch = db.Column(db.Integer, nullable=True)
     
     papertype = db.Column(db.String(120), nullable=True)
-    paper = db.Column(db.String(120), nullable=False)
+    paper = db.Column(db.LargeBinary, nullable=False)
+    def __repr__(self):
+        return 'data is : ' + str(self.sno)
    
 class sem3(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     batch = db.Column(db.Integer, nullable=True)
     
     papertype = db.Column(db.Integer, nullable=True)
-    paper = db.Column(db.Integer, nullable=False)
+    paper = db.Column(db.LargeBinary, nullable=False)
+    def __repr__(self):
+        return 'data is : ' + str(self.sno)
 class sem4(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     batch = db.Column(db.Integer, nullable=True)
     
     papertype = db.Column(db.String(120), nullable=True)
-    paper = db.Column(db.String(120), nullable=False)
+    paper = db.Column(db.LargeBinary, nullable=False)
+    def __repr__(self):
+        return 'data is : ' + str(self.sno)
 class sem5(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     batch = db.Column(db.Integer, nullable=True)
     
-    papertype = db.Column(db.Integer, nullable=True)
-    paper = db.Column(db.String(120), nullable=False)
+    papertype = db.Column(db.String(120), nullable=True)
+    paper = db.Column(db.LargeBinary, nullable=False)
+    def __repr__(self):
+        return 'data is : ' + str(self.sno)
 class sem6(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     batch = db.Column(db.Integer, nullable=True)
     
     papertype = db.Column(db.String(120), nullable=True)
-    paper = db.Column(db.String(120), nullable=False)
+    paper = db.Column(db.LargeBinary, nullable=False)
+    def __repr__(self):
+        return 'data is : ' + str(self.sno)
 
 class sem7(db.Model):
 
@@ -57,14 +69,17 @@ class sem7(db.Model):
     batch = db.Column(db.Integer, nullable=True)
     
     papertype = db.Column(db.String(120), nullable=True)
-    paper = db.Column(db.String(120), nullable=False)
+    paper = db.Column(db.LargeBinary, nullable=False)
+    def __repr__(self):
+        return 'data is : ' + str(self.sno)
 class sem8(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     batch = db.Column(db.Integer, nullable=True)
     
     papertype = db.Column(db.String(120), nullable=True)
-    paper = db.Column(db.String(120), nullable=False)
-
+    paper = db.Column(db.LargeBinary, nullable=False)
+    def __repr__(self):
+        return 'data is : ' + str(self.sno)
     
 @app.route("/")
 def home():
@@ -95,7 +110,8 @@ def return_sem():
     if str(radio_val)=='upload':
         return render_template('uploadtemplate.html',param=str(select),param1=str(radio_val))
     elif str(radio_val=='download'):
-        return render_template('paper_show.html',param=str(select),param1=str(radio_val))
+        all_papers=query.filter_by().all()
+        return render_template('paper_show.html',param=all_papers)
 def database_commit(entry):
     db.create_all()  
     db.session.add(entry)
@@ -106,7 +122,7 @@ def upload_papertodb():
     batch_get = request.form['batch']
     papertype_get = request.form['papertype']
     
-    paper_file = request.form['paper']
+    paper_file = request.files['paper'].read()#request.files['paper']
     global save_Sem_Status
     save_Sem_Status=sem_keep
     print("save sem is ",save_Sem_Status)
@@ -114,6 +130,7 @@ def upload_papertodb():
     if(request.method=='POST' and save_Sem_Status=='semster1' ):
         
         entry = sem1(batch=batch_get,papertype=papertype_get,paper=paper_file)
+        db.session.add(entry)
         database_commit(entry) #this fxn saves overhead below ifelse simply reduce no of lines
        
         return redirect("/getpapers")
@@ -121,32 +138,40 @@ def upload_papertodb():
 
     elif(request.method=='POST' and save_Sem_Status=='semster2' ):
         entry = sem2(batch=batch_get,papertype=papertype_get,paper=paper_file)
+        db.session.add(entry)
         database_commit(entry) #this fxn saves overhead below ifelse
         return redirect("/getpapers") 
     elif(request.method=='POST' and save_Sem_Status=='semster3' ):
         entry = sem3(batch=batch_get,papertype=papertype_get,paper=paper_file)
+        db.session.add(entry)
         database_commit(entry) #this fxn saves overhead below ifelse
         return redirect("/getpapers") 
     elif(request.method=='POST' and save_Sem_Status=='semster4' ):
         entry = sem4(batch=batch_get,papertype=papertype_get,paper=paper_file)
+        db.session.add(entry)
         database_commit(entry) #this fxn saves overhead below ifelse
         return redirect("/getpapers") 
     elif(request.method=='POST' and save_Sem_Status=='semster5' ):
         entry = sem5(batch=batch_get,papertype=papertype_get,paper=paper_file)
+        db.session.add(entry)
         database_commit(entry) #this fxn saves overhead below ifelse
         return redirect("/getpapers")   
     elif(request.method=='POST' and save_Sem_Status=='semster6' ):
         entry = sem6(batch=batch_get,papertype=papertype_get,paper=paper_file)
+        db.session.add(entry)
         database_commit(entry) #this fxn saves overhead below ifelse
         return redirect("/getpapers")    
     
     elif(request.method=='POST' and save_Sem_Status=='semster7' ):
-        entry = sem7(batch=batch_get,papertype=papertype_get,paper=paper_file)
+        entry = sem7(batch=batch_get,papertype=papertype_get)
+        db.session.add(entry)
         database_commit(entry) #this fxn saves overhead below ifelse
         return redirect("/getpapers") 
     elif(request.method=='POST' and save_Sem_Status=='semster8' ):
         entry = sem8(batch=batch_get,papertype=papertype_get,paper=paper_file)
+        db.session.add(entry)
         database_commit(entry) #this fxn saves overhead below ifelse
         return redirect("/getpapers") 
+    
 if __name__ == "__main__":
     app.run(debug=True,port=1125)    
